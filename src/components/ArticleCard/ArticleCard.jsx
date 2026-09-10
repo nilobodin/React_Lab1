17a5796ec0ca1d8955f86948eedf950f3bd95095
Nikita Lobodin <nikita.lobodin2005@mail.ru>
2026-09-10 13:13:10 +0600

    Add 2 card and images structure

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,11 +1,12 @@
 import style from './ArticleCard.module.css';
 
 function ArticleCard({ article }) {
-    const { title, excerpt, author, date, tag } = article;
+    const { title, excerpt, author, date, tag, image } = article;
     return (
         <article className={style.card}>
             <div className={style.body}>
                 <span className={style.tag}>{tag}</span>
+                <img src={image} alt={tag} className={style.img} />
                 <h2 className={style.title}>{title}</h2>
                 <p className={style.excerpt}>{excerpt}</p>
                 <footer className={style.footer}>
--- src/data/articles.js
+++ src/data/articles.js
@@ -1,52 +1,80 @@
 const articles = [
-    {
-        id: 1,
-        title: 'Что такое React',
-        excerpt: 'React - это библиотека для создания пользовательских интерфейсов. Рассматриваем основные концепции.',
-        author: 'Анна Ивановна',
-        date: '12 марта 2026',
-        tag: 'Основы',
-    },
-    {
-        id: 2,
-        title: 'Хуки в React',
-        excerpt: 'useState, useEffect, useContext - разбираемся, как работают хуки и зачем они нужны',
-        author: 'Петр Смирнов',
-        date: '18 марта 2026',
-        tag: 'Хуки',
-    },
-    {
-        id: 3,
-        title: 'Vite против Webpack',
-        excerpt: 'Сравниваем два сборщика: скорость, конфигурация и удобство разработки.',
-        author: 'Мария Кузнецова',
-        date: '22 марта 2026',
-        tag: 'Инструменты'
-    },
-    {
-        id: 4,
-        title: 'CSS-модули в React',
-        excerpt: 'Как изолировать стили компонентов и забыть о конфликтах классов.',
-        author: 'Иван Петров',
-        date: '25 марта 2026',
-        tag: 'Стили'
-    },
-    {
-        id: 5,
-        title: 'Flexbox за 5 минут',
-        excerpt: 'Краткий гайд по Flexbox: основные свойства и типичные паттерны вёрстки',
-        author: 'Ольга Сидорова',
-        date: '30 марта 2026',
-        tag: 'Вёрстка'
-    },
-    {
-        id: 6,
-        title: 'Git для фронтендера',
-        excerpt: 'Базовые команды Git, ветвление и публикация проекта на GitHub',
-        author: 'Дмитрий Волков',
-        date: '2 апреля 2026',
-        tag: 'Git'
-    }
-]
+  {
+    id: 1,
+    title: "Что такое React",
+    excerpt:
+      "React - это библиотека для создания пользовательских интерфейсов. Рассматриваем основные концепции.",
+    author: "Анна Ивановна",
+    date: "12 марта 2026",
+    tag: "Основы",
+    image: "./assets/babizana.png"
+  },
+  {
+    id: 2,
+    title: "Хуки в React",
+    excerpt:
+      "useState, useEffect, useContext - разбираемся, как работают хуки и зачем они нужны",
+    author: "Петр Смирнов",
+    date: "18 марта 2026",
+    tag: "Хуки",
+    image: "assets/babizana.png"
+  },
+  {
+    id: 3,
+    title: "Vite против Webpack",
+    excerpt:
+      "Сравниваем два сборщика: скорость, конфигурация и удобство разработки.",
+    author: "Мария Кузнецова",
+    date: "22 марта 2026",
+    tag: "Инструменты",
+    image: "assets/babizana.png"
+  },
+  {
+    id: 4,
+    title: "CSS-модули в React",
+    excerpt: "Как изолировать стили компонентов и забыть о конфликтах классов.",
+    author: "Иван Петров",
+    date: "25 марта 2026",
+    tag: "Стили",
+    image: "assets/babizana.png"
+  },
+  {
+    id: 5,
+    title: "Flexbox за 5 минут",
+    excerpt:
+      "Краткий гайд по Flexbox: основные свойства и типичные паттерны вёрстки",
+    author: "Ольга Сидорова",
+    date: "30 марта 2026",
+    tag: "Вёрстка",
+    image: "assets/planet.png"
+  },
+  {
+    id: 6,
+    title: "Git для фронтендера",
+    excerpt: "Базовые команды Git, ветвление и публикация проекта на GitHub",
+    author: "Дмитрий Волков",
+    date: "2 апреля 2026",
+    tag: "Git",
+    image: "assets/planet.png"
+  },
+  {
+    id: 7,
+    title: "Git для фронтендера",
+    excerpt: "Базовые команды Git, ветвление и публикация проекта на GitHub",
+    author: "Дмитрий Волков",
+    date: "2 апреля 2026",
+    tag: "Git",
+    image: "assets/planet.png"
+  },
+  {
+    id: 8,
+    title: "Git для фронтендера",
+    excerpt: "Базовые команды Git, ветвление и публикация проекта на GitHub",
+    author: "Дмитрий Волков",
+    date: "2 апреля 2026",
+    tag: "Git",
+    image: "assets/planet.png"
+  },
+];
 
-export default articles
+export default articles;
